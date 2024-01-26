0a3893f4a1f7ecbe59aca95279c886f21ec543de
HaleYesilova <haleyesilova647@gmail.com>
2024-01-26 13:19:51 +0300

    ChatGpt Frontend code implemented in project

--- client/script.js
+++ client/script.js
@@ -1,1 +1,11 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
+const from = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element){
+  '...'
+}
